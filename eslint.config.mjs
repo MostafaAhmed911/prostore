20e04f7170ecc6ccf9edadb0b274822ba79d1ad1
Mostafa Ahmed <mostafaahmedp911s@gmail.com>
2025-08-31 01:14:40 +0300

    disable any errors in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,6 +22,11 @@ const eslintConfig = [
     ],
   }, // <-----------------
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // disable "any" errors
+    },
+  },
 ];
 
 export default eslintConfig;
