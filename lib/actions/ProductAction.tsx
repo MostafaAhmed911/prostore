0a60e9a9ca240d377807ed1db976d7431c1631be
Mostafa Ahmed <mostafaahmedp911s@gmail.com>
2025-08-30 23:17:37 +0300

    add changes

--- lib/actions/ProductAction.tsx
+++ lib/actions/ProductAction.tsx
@@ -1,14 +1,26 @@
 "use server";
 
 import { PrismaClient } from "@prisma/client";
-import { convertToPlainObject } from "../utils";
+// import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { prisma } from "@/db/prisma (1)";
 
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
+  // const prisma = new PrismaClient();
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
   });
-  return convertToPlainObject(data);
+  return data.map((product) => ({
+    ...product,
+    price: product.price.toString(),
+    rating: product.rating.toString(),
+  }));
+}
+
+// get single product by it's slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
 }
